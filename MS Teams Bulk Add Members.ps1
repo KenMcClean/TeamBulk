8fc529410d56c18def1430f8009c488ba35b1e61
KenMcClean <65670906+KenMcClean@users.noreply.github.com>
2020-11-17 13:58:55 -0800

    Add files via upload

--- MS Teams Bulk Add Members.ps1
+++ MS Teams Bulk Add Members.ps1
@@ -1,11 +1,10 @@
 ﻿$getAdminID = read-host "Enter the email address of the group administrator"
 #The person adding members to the group must be an admin of the group
 
-$Group_Hash = @{}
+$Group_Info_Array = @()
 #Declare a hash table in which the group values will be stored
 
 $incrementKey = 1
-#we need a way to increment the key as the user groups are iterated. we start at 1, not zero, because we're referencing keys instead of array entries
 
 connect-microsoftteams -AccountId $getAdminID
 #You need to initiate a connection to the Teams backend, using the ID of the administrator
@@ -19,17 +18,30 @@ foreach($userGroup in $userGroups){
 write-host $incrementKey". ",$userGroup.Displayname
 #for each of the groups to which the Admin belongs, list it and give it a number
 
-$Group_Hash.add($incrementKey, $userGroup.GroupId)
-#Add the group to a hash, along with a number to use as a key
+$GroupInfo= New-Object -TypeName PSObject -Property @{
+  GroupID = $userGroup.GroupId
+  GroupName = $userGroup.DisplayName
+}
+#Create a new PS Custom Object containing the group ID and name
+
+$Group_Info_Array += $GroupInfo
+#Add the info to an array, to be referenced later
 
 $incrementKey++
 }
 
-$hashSelect = read-host "`r`nSelect which group new members should be added to"
-#get the key from the user
 
-$selection = $Group_Hash.[int]$hashselect
-#using the key from the user, retrieve the group id with a key that matches what the user put in
+do{
+#Solict input from the user regarding to which Team new members should be added
+try
+{$groupSelect = read-host "`r`nSelect to which Team new members should be added"}
+catch{"Please enter a valid selection"}
+}
+while(($groupSelect -notmatch "^[1-5]?[0-9]$") -or ($groupSelect -gt $Group_Info_Array.count))
+#Picked 59 as the arbitrary upper limit, and 1 as the lower limit. The input also cannot exceed the number of elements in the array
+
+$groupSelect -= 1
+#reduce the selection by 1 to account for an off-by-1 error
 
 write-host "`r`nSelect a text file containing the email addresses of the users to be added"
 $dialog = New-Object -TypeName System.Windows.Forms.OpenFileDialog
@@ -54,8 +66,31 @@ if ($result = 'OK')
     #get the content of the list
 } 
 
-foreach($groupMember in $groupMembers){
-write-host "Adding user"$groupMember "to the Team with a group id of" $selection
-#Tell the user what is happening
-#add-teamuser -GroupId $selection -user $groupMember
+do{
+#Solict input from the user, confirming that they want to make these changes
+try
+{$confirmation = read-host "Are you sure you want to add these users to ",$Group_Info_Array[$groupSelect].GroupName,"(Y/N)?"
 }
+catch{"Please enter Y or N"}
+}
+while(("Y","N" -notcontains $confirmation))
+#this confirmation is not case sensitive
+
+if($confirmation -eq "Y"){
+foreach($groupMember in $groupMembers){
+write-host "Adding user $groupMember to" $Group_Info_Array[$groupSelect].GroupName, $Group_Info_Array[$groupSelect].groupID
+#Tell the user what is happening
+#add-teamuser -GroupId $Group_Info_Array[$groupSelect].GroupID -user $groupMember
+#Make the changes
+
+}
+}
+else
+{
+write-host "Exiting without making changes"
+exit
+#Exit without making changes, and without closing the window
+}
+exit
+#Exit without closing the window, after users have been added to the group
+#>
